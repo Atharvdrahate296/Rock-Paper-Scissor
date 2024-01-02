35072df6007456acd1a482c4a285b37a9a2a0ade
Atharv Rahate <145426822+Atharvdrahate296@users.noreply.github.com>
2024-01-02 14:56:12 +0530

    Add files via upload

---
+++ Snake_water_gun.py
@@ -0,0 +1,33 @@
+#snake beats water , water beats gun and gun beats snake
+import random
+def game(comp,mine):                #game logic
+    if(comp =='S' and mine == "G"):
+        return True
+    elif(comp =="W" and mine == "S"):
+        return True
+    elif(comp =="G" and mine == "W"):
+        return True
+    else:
+        return False
+
+choice=('S','W','G')        #random choice by computer
+comp=random.randint(0,2)
+comp=choice[comp]
+
+mine=input("Choose either S(snake) , W(water) or G(gun) :")    #player input
+print(f"You choose : {mine}")
+print(f"Computer choose : {comp}")
+
+if(mine != "S" and mine != "G" and mine != "W"):  #error management
+    print("Incorrect choice.")
+    exit()
+elif(mine == comp ):
+    print("Draw!!")
+    exit()
+
+
+win=game(comp,mine)             #passing both choices to evaluate
+if(win):                        #if true:you win , else :loose
+    print("You Won!")
+else:
+    print("You lose")
